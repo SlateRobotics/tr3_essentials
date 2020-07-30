e489dbb986f6326af52323ef6422a6331a33a004
Zach Allen <zach@slaterobotics.com>
2020-07-29 20:42:51 -0500

    tabs to spaces

--- tr3py/src/tr3py/tr3.py
+++ tr3py/src/tr3py/tr3.py
@@ -23,74 +23,74 @@ TAU = math.pi * 2.0
 
 close = False
 def signal_handler(sig, frame):
-	global close
-	close = True
-	sys.exit(0)
+    global close
+    close = True
+    sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
 
 class TR3:
-	_msgs = tr3_msgs.Msgs()
+    _msgs = tr3_msgs.Msgs()
 
-	_state = None
-	state_change = None
+    _state = None
+    state_change = None
 
-	mode_servo = 0x10
-	mode_backdrive = 0x11
-	mode_rotate = 0x12
+    mode_servo = 0x10
+    mode_backdrive = 0x11
+    mode_rotate = 0x12
 
-	def __init__(self):
-		self.b0 = Joint(self, "b0")
-		self.b1 = Joint(self, "b1")
+    def __init__(self):
+        self.b0 = Joint(self, "b0")
+        self.b1 = Joint(self, "b1")
         self.a0 = Joint(self, "a0")
-		self.a1 = Joint(self, "a1")
-		self.a2 = Joint(self, "a2")
-		self.a3 = Joint(self, "a3")
-		self.a4 = Joint(self, "a4")
-		self.g0 = Joint(self, "g0")
-		self.h0 = Joint(self, "h0")
-		self.h1 = Joint(self, "h1")
+        self.a1 = Joint(self, "a1")
+        self.a2 = Joint(self, "a2")
+        self.a3 = Joint(self, "a3")
+        self.a4 = Joint(self, "a4")
+        self.g0 = Joint(self, "g0")
+        self.h0 = Joint(self, "h0")
+        self.h1 = Joint(self, "h1")
         self.p0 = Joint(self, "p0")
-		self.p1 = Joint(self, "p1")
+        self.p1 = Joint(self, "p1")
         self.p2 = Joint(self, "p2")
         self._msgs.state_change = self.handle_state_change
 
-		print("TR3 waiting for state")
-		while self._state == None:
-			self.step()
+        print("TR3 waiting for state")
+        while self._state == None:
+            self.step()
 
         self.powerup()
         print("TR3 ready")
 
-	def handle_state_change(self, state):
-		if self.state_change != None:
-			self.state_change(state)
+    def handle_state_change(self, state):
+        if self.state_change != None:
+            self.state_change(state)
 
-	def state(self):
-		return self._state
+    def state(self):
+        return self._state
 
-	def drive(self, motorLeft, motorRight, motorDuration = 250):
-		offsetBinary = 100
+    def drive(self, motorLeft, motorRight, motorDuration = 250):
+        offsetBinary = 100
 
-		packet = tr3_msgs.Packet()
-		packet.address = "b0"
-		packet.cmd = CMD_ROTATE
-		packet.addParam(int((motorLeft * 100.0) + offsetBinary))
-		packet.addParam(int((motorRight * 100.0) + offsetBinary))
-		packet.addParam(int(math.floor(motorDuration % 256)))
-		packet.addParam(int(math.floor(motorDuration / 256)))
+        packet = tr3_msgs.Packet()
+        packet.address = "b0"
+        packet.cmd = CMD_ROTATE
+        packet.addParam(int((motorLeft * 100.0) + offsetBinary))
+        packet.addParam(int((motorRight * 100.0) + offsetBinary))
+        packet.addParam(int(math.floor(motorDuration % 256)))
+        packet.addParam(int(math.floor(motorDuration / 256)))
 
-		self._msgs.add(packet)
-		self.step()
+        self._msgs.add(packet)
+        self.step()
 
-	def release(self):
-		self.a0.release()
-		self.a1.release()
-		self.a2.release()
-		self.a3.release()
-		self.a4.release()
-		self.h0.release()
-		self.h1.release()
+    def release(self):
+        self.a0.release()
+        self.a1.release()
+        self.a2.release()
+        self.a3.release()
+        self.a4.release()
+        self.h0.release()
+        self.h1.release()
 
     def powerup(self):
         self.p0.setPosition(1)
@@ -107,62 +107,62 @@ class TR3:
         self.sleep(2)
         self.close()
 
-	def stop(self):
-		self.a0.stop()
-		self.a1.stop()
-		self.a2.stop()
-		self.a3.stop()
-		self.a4.stop()
-		self.h0.stop()
-		self.h1.stop()
+    def stop(self):
+        self.a0.stop()
+        self.a1.stop()
+        self.a2.stop()
+        self.a3.stop()
+        self.a4.stop()
+        self.h0.stop()
+        self.h1.stop()
 
-	def setMode(self, mode):
-		self.a0.setMode(mode)
-		self.a1.setMode(mode)
-		self.a2.setMode(mode)
-		self.a3.setMode(mode)
-		self.a4.setMode(mode)
-		self.h0.setMode(mode)
-		self.h1.setMode(mode)
+    def setMode(self, mode):
+        self.a0.setMode(mode)
+        self.a1.setMode(mode)
+        self.a2.setMode(mode)
+        self.a3.setMode(mode)
+        self.a4.setMode(mode)
+        self.h0.setMode(mode)
+        self.h1.setMode(mode)
 
-	def sleep(self, sec):
-		t_start = time.time()
-		while time.time() - t_start < sec:
-			self.step()
+    def sleep(self, sec):
+        t_start = time.time()
+        while time.time() - t_start < sec:
+            self.step()
 
-	def getJoint(self, id):
-		try:
-			return getattr(self,id)
-		except:
-			pass
+    def getJoint(self, id):
+        try:
+            return getattr(self,id)
+        except:
+            pass
 
-	def step(self):
-		global close
-		if close == True:
-			self._msgs.close()
-			self.close()
-			return
+    def step(self):
+        global close
+        if close == True:
+            self._msgs.close()
+            self.close()
+            return
 
-		self._msgs.step()
-		self._state = self._msgs.state()
+        self._msgs.step()
+        self._state = self._msgs.state()
 
-		if self._state == None:
-			return
+        if self._state == None:
+            return
 
-		ids, states = self._state
-		for i in range(len(ids)):
-			try:
-				getattr(self,ids[i])._state = states[i]
-			except:
-				pass
+        ids, states = self._state
+        for i in range(len(ids)):
+            try:
+                getattr(self,ids[i])._state = states[i]
+            except:
+                pass
 
-	def spin(self, condition = True):
-		global close
-		while condition == True or close == True:
-			self.step()
+    def spin(self, condition = True):
+        global close
+        while condition == True or close == True:
+            self.step()
 
-	def close(self):
+    def close(self):
         self.p0.setPosition(0)
-		self.sleep(1)
-		while self._msgs._msgs != "":
-			self.step()
+        self.sleep(1)
+        while self._msgs._msgs != "":
+            self.step()
